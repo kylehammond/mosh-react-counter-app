f57cb6123359ec00ad899c182d623d47af49703c
Kyle Hammond <kyle.hammond@gmail.com>
2021-09-25 10:49:11 -0500

    set style directly

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,7 +13,7 @@ class Counter extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
-				<span style={this.styles} className="badge badge-primary m-2">
+				<span style={{ fontWeight: 30 }} className="badge badge-primary m-2">
 					{this.formatCount()}
 				</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
