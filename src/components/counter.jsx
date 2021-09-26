a7bd92ef5ae0660362dbb3a635cdbc21a734949c
Kyle Hammond <kyle.hammond@gmail.com>
2021-09-25 20:32:16 -0500

    my own desctructuring

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,19 +2,20 @@ import React, { Component } from "react";
 
 class Counter extends React.Component {
 	render() {
+		const { counter, onIncrement, onDelete } = this.props;
 		return (
 			<div>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button
 					onClick={() => {
-						this.props.onIncrement(this.props.counter);
+						onIncrement(counter);
 					}}
 					className="btn btn-secondary btn-sm">
 					Increment
 				</button>
 				<button
 					onClick={() => {
-						this.props.onDelete(this.props.counter.id);
+						onDelete(counter.id);
 					}}
 					className="btn btn-danger btn-sm m-2">
 					Delete
@@ -24,13 +25,15 @@ class Counter extends React.Component {
 	}
 
 	formatCount() {
-		const { value: count } = this.props.counter;
+		const { counter } = this.props;
+		const { value: count } = counter;
 		return count === 0 ? "Zero" : count;
 	}
 
 	getBadgeClasses() {
+		const { counter } = this.props;
 		let classes = "badge m-2 badge-";
-		classes += this.props.counter.value === 0 ? "warning" : "primary";
+		classes += counter.value === 0 ? "warning" : "primary";
 		return classes;
 	}
 }
